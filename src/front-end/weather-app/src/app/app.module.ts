2269ab3650058d051d8eb5ab57c873e67cba38b2
Nemtrack <balazsgyenes10@gmail.com>
2023-09-21 14:48:01 +0200

    Add HttpClient to app.module.ts

--- src/front-end/weather-app/src/app/app.module.ts
+++ src/front-end/weather-app/src/app/app.module.ts
@@ -4,11 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { CardComponent } from './card/card.component';
 import { FormsModule } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent, CardComponent],
   imports: [BrowserModule, FormsModule],
-  providers: [],
+  providers: [HttpClient],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
