33fd9883bfc8d4e501a8efa21d1917c393da127b
Nemtrack <balazsgyenes10@gmail.com>
2023-09-21 12:34:59 +0200

    Added card model, refactoring

--- src/front-end/weather-app/src/app/card/card-data.service.ts
+++ src/front-end/weather-app/src/app/card/card-data.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@angular/core';
+import { Card } from './card.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CardDataService {
+
+  cards: Card[] = [];
 
   constructor() { }
 }
