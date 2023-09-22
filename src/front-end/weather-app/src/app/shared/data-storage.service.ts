7839c78ccfc3955b86da50c6aa2477000c726f18
Exanim <simonbarath7@gmail.com>
2023-09-22 17:06:41 +0200

    Modify fetch logic to return only necessary info

--- src/front-end/weather-app/src/app/shared/data-storage.service.ts
+++ src/front-end/weather-app/src/app/shared/data-storage.service.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { map } from 'rxjs';
+import { DataList } from './list.model';
+import { getModel } from './get.model';
 
 @Injectable({
   providedIn: 'root',
@@ -29,12 +31,26 @@ export class DataStorageService {
       .subscribe((filteredData) => {
         this.lat = filteredData.lat;
         this.lon = filteredData.lon;
+        console.log(this.lat, this.lon);
         this.getWeatherInformationByGeoData();
       });
   }
 
   getWeatherInformationByGeoData() {
     const forecastUrl = `http://api.openweathermap.org/data/2.5/forecast?lat=${this.lat}&lon=${this.lon}&appid=${this.apiKey}`;
-    this.http.get(forecastUrl).subscribe((response) => console.log(response));
+    this.http
+      .get<any>(forecastUrl)
+      .pipe(
+        map((response) =>
+          response.list.map((item: any) => ({
+            dt_txt: item.dt_txt,
+            temp: item.main.temp,
+            humidity: item.main.humidity,
+            weather: item.weather[0].main,
+            windSpeed: item.wind.speed,
+          }))
+        )
+      )
+      .subscribe((filteredResponse) => console.log(filteredResponse));
   }
 }
