e0d21ff33440a4360044f0d6055e3c0941d5edde
Balázs Gyenes <balazsgyenes10@gmail.com>
2023-09-22 23:32:40 +0200

    Add types to respective GET answers

--- src/front-end/weather-app/src/app/shared/data-storage.service.ts
+++ src/front-end/weather-app/src/app/shared/data-storage.service.ts
@@ -1,14 +1,15 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { map } from 'rxjs';
-import { DataList } from './list.model';
-import { getModel } from './get.model';
+import { Weather } from './weather.model';
+import { GetAll, List } from './get-all.model';
+import { CardDataService } from '../card/card-data.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class DataStorageService {
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private cardData: CardDataService) {}
 
   apiKey = '8d9445cab37f8906d9588c8b9977dd08';
   lat = 0;
@@ -39,18 +40,26 @@ export class DataStorageService {
   getWeatherInformationByGeoData() {
     const forecastUrl = `http://api.openweathermap.org/data/2.5/forecast?lat=${this.lat}&lon=${this.lon}&appid=${this.apiKey}`;
     this.http
-      .get<any>(forecastUrl)
+      .get<GetAll>(forecastUrl)
       .pipe(
         map((response) =>
-          response.list.map((item: any) => ({
-            dt_txt: item.dt_txt,
-            temp: item.main.temp,
-            humidity: item.main.humidity,
-            weather: item.weather[0].main,
-            windSpeed: item.wind.speed,
-          })).filter((item: any) => item.dt_txt.charAt(11) === '1' && item.dt_txt.charAt(12) === '2')
+          response.list
+            .map((item: List) => ({
+              dt_txt: item.dt_txt,
+              temp: item.main.temp,
+              humidity: item.main.humidity,
+              weather: item.weather[0].main,
+              windSpeed: item.wind.speed,
+            }))
+            .filter(
+              (item: Weather) =>
+                item.dt_txt.charAt(11) === '1' && item.dt_txt.charAt(12) === '2'
+            )
         )
       )
-      .subscribe((filteredResponse) => console.log(filteredResponse));
+      .subscribe((filteredResponse) => {
+        console.log(filteredResponse);
+        this.cardData.setCardsData(filteredResponse);
+      });
   }
 }
