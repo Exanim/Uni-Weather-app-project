d61a6dec1683d7eee0fb6a5ad22714e0e3bbdb83
Balázs Gyenes <balazsgyenes10@gmail.com>
2023-09-24 15:50:40 +0200

    Add and refactor tests

--- src/front-end/weather-app/src/app/app.component.spec.ts
+++ src/front-end/weather-app/src/app/app.component.spec.ts
@@ -1,24 +1,81 @@
-import { TestBed } from "@angular/core/testing";
+import { ComponentFixture, TestBed, async } from "@angular/core/testing";
 import { AppComponent } from "./app.component";
 import { HttpClientModule } from "@angular/common/http";
+import { FormsModule, NgForm } from "@angular/forms";
 
 describe('Component: App', () => {
-  beforeEach(() => {
-    TestBed.configureTestingModule({
+
+  let fixture: ComponentFixture<AppComponent>;
+  let app: AppComponent;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
       declarations: [AppComponent],
-      imports: [HttpClientModule]
-    })
+      imports: [FormsModule, HttpClientModule]
+    }).compileComponents();
+    fixture = TestBed.createComponent(AppComponent);
+    app = fixture.componentInstance;
   })
 
   it('should create the app', () =>{
-    let fixture = TestBed.createComponent(AppComponent);
-    let app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   })
 
   it('should have the correct title', ()=>{
-    let fixture = TestBed.createComponent(AppComponent);
-    let app = fixture.debugElement.componentInstance;
     expect(app.title).toBe('Weather Webapp');
   })
+
+  it('should have a definition of array: "cards"', ()=>{
+    expect(app.cards).toBeDefined();
+  })
+
+  it('should have a definition of city', ()=>{
+    expect(app.city).toBeDefined();
+  })
+
+  it('should have a definition of showEachPack', ()=>{
+    expect(app.showEachPack).toBeDefined();
+  })
+
+  it('should have a definition of formerCity', ()=>{
+    expect(app.formerCity).toBeDefined();
+  })
+
+  it('should have a definition of isFetching', ()=>{
+    expect(app.isFetching).toBeDefined();
+  })
+
+  it('form should not submit if input empty', () => {
+    fixture.detectChanges();
+  
+    const cityInput: HTMLInputElement = fixture.debugElement.nativeElement.querySelector('#city');
+    const form = fixture.nativeElement.querySelector('form');
+    cityInput.value = '';
+  
+    spyOn(app, 'onLookUpCity');
+    form.dispatchEvent(new Event('submit'));
+  
+    fixture.whenStable().then(() => {
+      expect(app.onLookUpCity).not.toHaveBeenCalled();
+    });
+  });
+  
+
+  // it('should call onLookUpCity when the form is submitted', async () => {
+  //   let fixture = TestBed.createComponent(AppComponent);
+  //   let app = fixture.debugElement.componentInstance;
+  //   spyOn(app, 'onLookUpCity');
+  
+  //   app.city = 'Budapest'; // Állítsa be a city értékét először
+    
+  //   const form = fixture.nativeElement.querySelector('form');
+  //   form.dispatchEvent(new Event('submit'));
+    
+  //   await fixture.whenStable(); // Várjon az aszinkron művelet végére
+    
+    
+  //   fixture.detectChanges();
+  //   expect(app.formerCity).toBe('Budapest');
+  //   // Itt ellenőrizheti az egyéb elvárt eredményeket vagy viselkedést
+  // });
 });
