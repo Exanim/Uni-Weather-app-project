7f519969af60ee353e880abfe4b3a86e79f32752
Balázs Gyenes <balazsgyenes10@gmail.com>
2023-09-24 12:12:14 +0200

    Add first unit test to check if the app gets created

---
+++ src/front-end/weather-app/src/app/app.component.spec.ts
@@ -0,0 +1,18 @@
+import { TestBed } from "@angular/core/testing";
+import { AppComponent } from "./app.component";
+import { HttpClientModule } from "@angular/common/http";
+
+describe('Component: App', () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [AppComponent],
+      imports: [HttpClientModule]
+    })
+  })
+
+  it('should create the app', () =>{
+    let fixture = TestBed.createComponent(AppComponent);
+    let app = fixture.debugElement.componentInstance;
+    expect(app).toBeTruthy();
+  })
+});
